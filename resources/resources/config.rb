304da3aa0c42d92b6217b0fd0289d5a8de7b46f7
Miguel Alvarez <malvarez@redborder.com>
2025-02-27 12:33:29 +0000

    Fix lint

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -9,6 +9,6 @@ attribute :log_dir, kind_of: String, default: '/var/log/rb-druid-indexer'
 attribute :user, kind_of: String, default: 'rb-druid-indexer'
 attribute :zookeeper_servers, kind_of: Array, default: ['zookeeper.service:2181']
 attribute :tasks, kind_of: Array, default: [
-  { task_name: "rb_monitor", namespace: "", feed: "rb_monitor", kafka_host: "kafka.service:9092" },
-  { task_name: "rb_flow", namespace: "", feed: "rb_flow_post", kafka_host: "kafka.service:9092" }
+  { task_name: 'rb_monitor', namespace: '', feed: 'rb_monitor', kafka_host: 'kafka.service:9092' },
+  { task_name: 'rb_flow', namespace: '', feed: 'rb_flow_post', kafka_host: 'kafka.service:9092' },
 ]
