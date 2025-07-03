102bfe1bd0eddababec4d8bd4745c7461a03bfe5
Rafael Gomez <rgomez@redborder.com>
2025-07-03 10:19:18 +0100

    Remove unused attributes from config resource

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -8,6 +8,4 @@ attribute :config_dir, kind_of: String, default: '/etc/rb-druid-indexer'
 attribute :log_dir, kind_of: String, default: '/var/log/rb-druid-indexer'
 attribute :user, kind_of: String, default: 'rb-druid-indexer'
 attribute :zk_hosts, kind_of: Array
-attribute :kafka_brokers, kind_of: Array
-attribute :namespaces, kind_of: Array, default: []
 attribute :tasks, kind_of: Array, default: []
